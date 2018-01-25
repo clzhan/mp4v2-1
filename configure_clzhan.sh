f8d4a53080b782aad714764cc912b794576d9fef
root <root@localhost.localdomain>
2018-01-25 14:15:10 +0800

    add demo
    Signed-off-by: root <root@localhost.localdomain>

---
+++ configure_clzhan.sh
@@ -0,0 +1,1 @@
+./configure --prefix=/root/clzhan/oschina/mp4v2-1/clzhan --with-pic  
--- include/mp4v2/project.h
+++ include/mp4v2/project.h
@@ -24,7 +24,7 @@
 #define MP4V2_PROJECT_repo_rev        493
 #define MP4V2_PROJECT_repo_date       "2012-05-20 15:16:54 -0700 (Sun, 20 May 2012)"
 #define MP4V2_PROJECT_repo_type       "stable"
-#define MP4V2_PROJECT_build           "Tue Nov 22 10:10:06 EST 2016"
+#define MP4V2_PROJECT_build           "Thu Jan 25 14:14:32 CST 2018"
 
 /*****************************************************************************/
 
